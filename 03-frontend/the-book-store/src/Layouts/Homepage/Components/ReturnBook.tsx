89fca489a1f0a2eeff40694d7966a15dbb11b786
Johnny Wells <johnnyw01github@gmail.com>
2023-05-12 14:44:03 -0500

    Corrected the file pathway for the img in the ReturnBook component.

--- 03-frontend/the-book-store/src/Layouts/Homepage/Components/ReturnBook.tsx
+++ 03-frontend/the-book-store/src/Layouts/Homepage/Components/ReturnBook.tsx
@@ -6,7 +6,7 @@ export const ReturnBook = () => {
         <div className='col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3'>
             <div className='text-center'>
                 <img
-                    src={require('/Users/johnnywells/Desktop/Library-App/03-frontend/react-library/src/Images/BooksImages/book-luv2code-1000.png')}
+                    src={require('/Users/johnnywells/IdeaProjects/The-Book-Store-Web-App/03-frontend/the-book-store/src/Images/BookImages/book-luv2code-1000.png')}
                     width='151'
                     height='233'
                     alt="book"
