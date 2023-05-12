e94281877982badb07681ed9269542026c41dbfb
Johnny Wells <johnnyw01github@gmail.com>
2023-05-12 13:52:40 -0500

    Created the navbar component for the application.

--- 03-frontend/the-book-store/src/App.tsx
+++ 03-frontend/the-book-store/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Navbar} from "./Layouts/NavbarAndFooter/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
 
     </div>
   );
