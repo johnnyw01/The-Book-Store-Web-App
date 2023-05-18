61b232f5b801065401619dc52db37149293b4c74
Johnny Wells <johnnyw01github@gmail.com>
2023-05-18 10:07:30 -0500

    Added search funcitonality to the BookRepository so that the Restful API can access the books in the database by their titles.

--- 02-backend/spring-boot-book-store/src/main/java/com/thebookstore/springbootbookstore/dao/BookRepository.java
+++ 02-backend/spring-boot-book-store/src/main/java/com/thebookstore/springbootbookstore/dao/BookRepository.java
@@ -1,7 +1,11 @@
 package com.thebookstore.springbootbookstore.dao;
 
 import com.thebookstore.springbootbookstore.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository <Book, Long>{
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
